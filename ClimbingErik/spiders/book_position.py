95e68ff0986a14e28e747266f623f571aba80453
erik <szwangkexun@126.com>
2019-12-11 09:36:51 +0800

    新增模拟浏览器

--- ClimbingErik/spiders/book_position.py
+++ ClimbingErik/spiders/book_position.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
-import scrapy
+import scrapy,time
 from ClimbingErik.items import ClimbingerikItem
+from selenium import webdriver
 class BookPositionSpider(scrapy.Spider):
+    browser=webdriver.Firefox(executable_path="browser/geckodriver")
+    time.sleep(3)
+    browser.get("http://xbiquge.la/xiaoshuodaquan/")
+    time.sleep(5)
     name = 'book_position'
     allowed_domains = ['xbiquge.la']
     start_urls = ['http://xbiquge.la/xiaoshuodaquan/']
